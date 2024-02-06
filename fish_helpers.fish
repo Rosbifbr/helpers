006c350346589881fc2fa0c5e04787e3cf34a4d1
Rodrigo Ourique <rodrigo.guima.ourique@gmail.com>
2024-02-06 14:51:59 -0300

    Init

---
+++ fish_helpers.fish
@@ -0,0 +1,21 @@
+#!/usr/bin/fish
+#Misc fish helpers
+#Copyright Rodrigo Ourique @ 2024. MIT License
+
+function ff -d "Fuzzy find files"
+    set final ".*"
+    for elem in $argv
+        set final "$final$elem.*"
+    end
+
+    #Find file finally
+    find . -iregex $final
+end
+
+function logcat_search -d "Logcat into app process"
+    adb logcat --pid (adb shell ps | grep -Po "^[a-z0-9_]+\s+\K\d+(?=.*$argv)")
+end
+
+function android_emulator -d "Run predefined AVD in an Emulator (independent from Android Studio)"
+    /home/rodrigo/Android/Sdk/emulator/emulator -avd Weak_API_27
+end
